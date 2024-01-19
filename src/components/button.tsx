38d0cc8d9b0ce45b459894b27d1fa5d8d3766553
Zolastic <honhattienlul@gmail.com>
2024-01-19 20:19:24 +0800

    feat: Finished creating reusable button component

---
+++ src/components/button.tsx
@@ -0,0 +1,58 @@
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-slate-600 text-slate-600-foreground shadow hover:bg-slate-600/90",
+        destructive:
+          "bg-red-800 text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
+        primary:
+          "bg-blue-600 text-white hover:bg-blue-700 focus-visible:ring-blue-500",
+        secondary:
+          "bg-gray-200 text-gray-800 hover:bg-gray-300 focus-visible:ring-gray-500",
+        tertiary:
+          "bg-transparent text-gray-800 hover:bg-gray-100 focus-visible:ring-gray-500",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({
+  className,
+  size,
+  variant,
+  ...props
+}) {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props} // the children prop is passed here
+    />
+  );
+});
+
+// we can export the variants for use in other components.
+// This is useful for when we want to use the same variants in multiple components.
+// Making it even more reusable.
+export { Button, buttonVariants };
